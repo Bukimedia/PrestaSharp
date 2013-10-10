fa581ff0adfda55cb6f85b8f16b35e79553998eb
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-10 17:56:44 +0200

    Combinations in progress.

---
+++ Entities/combination.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PrestaSharp.Entities
+{
+    public class combination : PrestashopEntity
+    {
+
+        public long? id { get; set; }
+        public long? id_product { get; set; }
+        public string location { get; set; }
+        public string ean13 { get; set; }
+        public string upc { get; set; }
+        public int quantity { get; set; }
+        public string reference { get; set; }
+        public string supplier_reference { get; set; }
+        public decimal wholesale_price { get; set; }
+        public decimal price { get; set; }
+        public decimal ecotax { get; set; }
+        public decimal weight { get; set; }
+        public decimal unit_price_impact { get; set; }
+        public decimal minimal_quantity { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int default_on { get; set; }
+        /// <summary>
+        /// It´s a logical DateTime field. Format YYYY-MM-DD HH:MM:SS.
+        /// It´s string because you can receive a string with no DateTime .Net format.
+        /// </summary>
+        public string available_date { get; set; }
+        public AuxEntities.AssociationsProduct associations { get; set; }
+
+    }
+}
+
---
+++ Factories/CombinationFactory.cs
@@ -0,0 +1,73 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PrestaSharp.Factories
+{
+    public class CombinationFactory : RestSharpFactory
+    {
+        public CombinationFactory(string BaseUrl, string Account, string SecretKey)
+            : base(BaseUrl, Account, SecretKey)
+        {
+
+        }
+
+        public Entities.combination Get(int CombinationId)
+        {
+            RestRequest request = this.RequestForGet("combinations", CombinationId, "combination");
+            return this.Execute<Entities.combination>(request);
+        }
+
+        public Entities.combination Add(Entities.combination Combination)
+        {
+            Combination.id = null;
+            RestRequest request = this.RequestForAdd("combinations", Combination);
+            return this.Execute<Entities.combination>(request);
+        }
+
+
+        public void Update(Entities.combination Combination)
+        {
+            RestRequest request = this.RequestForUpdate("combinations", Combination.id, Combination);
+            this.Execute<Entities.combination>(request);
+        }
+
+        public void Delete(Entities.combination Combination)
+        {
+            RestRequest request = this.RequestForDelete("combinations", Combination.id);
+            this.Execute<Entities.combination>(request);
+        }
+
+        public List<int> GetIds()
+        {
+            RestRequest request = this.RequestForGet("combinations", null, "prestashop");
+            return this.ExecuteForGetIds<List<int>>(request, "combination");
+        }
+
+        /// <summary>
+        /// More information about filtering: http://doc.prestashop.com/display/PS14/Chapter+8+-+Advanced+Use
+        /// </summary>
+        /// <param name="Filter">Example: key:name value:Apple</param>
+        /// <param name="Sort">Field_ASC or Field_DESC. Example: name_ASC or name_DESC</param>
+        /// <param name="Limit">Example: 5 limit to 5. 9,5 Only include the first 5 elements starting from the 10th element.</param>
+        /// <returns></returns>
+        public List<Entities.combination> GetByFilter(Dictionary<string, string> Filter, string Sort, string Limit)
+        {
+            RestRequest request = this.RequestForFilter("combinations", "full", Filter, Sort, Limit, "combinations");
+            return this.Execute<List<Entities.combination>>(request);
+        }
+
+        /// <summary>
+        /// Get all combinations.
+        /// </summary>
+        /// <returns>A list of combinations</returns>
+        public List<Entities.combination> GetAll()
+        {
+            return this.GetByFilter(null, null, null);
+        }
+    }
+}
