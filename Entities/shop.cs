7d0e96a1ebf4bf5e459acea6940843aecf780444
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-13 16:49:48 +0200

    Added shops.

---
+++ Entities/shop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestaSharp.Entities
+{
+    public class shop : PrestashopEntity
+    {
+        public long? id { get; set; }
+        public long? id_shop_group { get; set; }
+        public long? id_category { get; set; }
+        public long? id_theme { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int active { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int deleted { get; set; }
+        public string name { get; set; }
+    }
+}
