a1744aa274ad868ee5101cfc7a4718f1606f08d2
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-16 11:08:40 +0200

    Issue #2 Deleted the Models namespace

--- Models/Prestashop.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Documentos\Visual Studio 2012\Projects\SexShopWorld para Prestashop\PrestaSharp\Models\Prestashop.edmx'. 
-// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
-// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
-// el modelo en el diseñador.
