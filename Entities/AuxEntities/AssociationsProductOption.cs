3934e3b8d58b7d395968718c0b1242547cb81c3f
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-19 17:27:42 +0200

    Added product_options

---
+++ Entities/AuxEntities/AssociationsProductOption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrestaSharp.Entities.AuxEntities
+{
+    public class AssociationsProductOption : PrestashopEntity
+    {
+        public List<AuxEntities.product_option_value> product_option_values { get; set; }
+    }
+}
