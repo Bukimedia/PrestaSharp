23476986f3a27400b08218034f191042783f7538
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-21 11:35:19 +0200

    Minor updates

--- Entities/product_option_value.cs
+++ Entities/product_option_value.cs
@@ -8,33 +8,12 @@ namespace PrestaSharp.Entities
 {
     public class product_option_value : PrestashopEntity
     {
+
         public long? id { get; set; }
         public long? id_attribute_group { get; set; }
         public string color { get; set; }
         public int position { get; set; }
         public List<PrestaSharp.Entities.AuxEntities.language> name { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            PrestaSharp.Entities.product_option_value Aux = (PrestaSharp.Entities.product_option_value)obj;
-            bool AreIdAttributeGroupEquals = (this.id_attribute_group == Aux.id_attribute_group);
-            bool ContainsLanguage = false;
-            for (int i = 0; i < this.name.Count(); i++)
-            {
-                foreach (PrestaSharp.Entities.AuxEntities.language Language in Aux.name)
-                {
-                    if (this.name[i].Value == Language.Value && this.name[i].id == Language.id)
-                    {
-                        ContainsLanguage = true;
-                    }
-                }
-            }
-            return (AreIdAttributeGroupEquals && ContainsLanguage);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
     }
 }
