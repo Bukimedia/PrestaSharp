cf9d3b284994e995045eae34bbe03ffbe7433ec8
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-21 23:04:29 +0200

    GetIdsByFilter methods added (Issue #4)

--- Factories/ProductFactory.cs
+++ Factories/ProductFactory.cs
@@ -62,6 +62,19 @@ namespace PrestaSharp.Factories
         }
 
         /// <summary>
+        /// More information about filtering: http://doc.prestashop.com/display/PS14/Chapter+8+-+Advanced+Use
+        /// </summary>
+        /// <param name="Filter">Example: key:name value:Apple</param>
+        /// <param name="Sort">Field_ASC or Field_DESC. Example: name_ASC or name_DESC</param>
+        /// <param name="Limit">Example: 5 limit to 5. 9,5 Only include the first 5 elements starting from the 10th element.</param>
+        /// <returns></returns>
+        public List<long?> GetIdsByFilter(Dictionary<string, string> Filter, string Sort, string Limit)
+        {
+            RestRequest request = this.RequestForFilter("products", "id", Filter, Sort, Limit, "products");
+            return this.Execute<List<long?>>(request);
+        }
+
+        /// <summary>
         /// Get all products.
         /// </summary>
         /// <returns>A list of products</returns>
