90b38b6151841b047e58cfa6e105583f567b2153
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-24 12:22:58 +0200

    Issue #10 fixed

--- Entities/AuxEntities/language.cs
+++ Entities/AuxEntities/language.cs
@@ -1,15 +1,17 @@
-﻿using RestSharp.Serializers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.Serialization;
+using RestSharp.Serializers;
+using System.Runtime.Serialization;
 
 namespace PrestaSharp.Entities.AuxEntities
 {
     [Serializable]
+    [DataContract]
     public class language: PrestashopEntity
     {
         public language()
@@ -24,10 +26,12 @@ namespace PrestaSharp.Entities.AuxEntities
 
 
         [XmlAttribute]
+        [DataMember]
         public long id { get; set; }
 
         // Value is reserved word from RestSharp for loading the CDATA content from the XML file.
         [XmlTextAttribute]
+        [DataMember]
         public string Value { get; set; }
 
     }
