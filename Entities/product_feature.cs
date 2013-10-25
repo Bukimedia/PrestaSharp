57d93662c0faf74568a627cf202a243239692bab
Eduardo <eduardo.leon.pavon@gmail.com>
2013-10-25 11:53:18 +0200

    Added product_features (Issue #11)

---
+++ Entities/product_feature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestaSharp.Entities
+{
+    public class product_feature : PrestashopEntity
+    {
+        public long? id { get; set; }
+        public int position { get; set; }
+        public List<PrestaSharp.Entities.AuxEntities.language> name { get; set; }
+
+        public product_feature()
+        {
+            this.name = new List<AuxEntities.language>();
+        }
+    }
+}
