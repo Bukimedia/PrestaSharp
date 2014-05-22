3bc3d27534034fe300fb3a9fc767258dcbabdd62
Eduardo <eduardo.leon.pavon@gmail.com>
2014-05-22 18:15:04 +0200

    Added tax_rule_group

---
+++ Entities/tax_rule_group.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PrestaSharp.Entities
+{
+    [XmlType(Namespace = "PrestaSharp/Entities")]
+    public class tax_rule_group : PrestashopEntity
+    {
+        public long? id { get; set; }
+        public string name { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int active { get; set; }
+
+        public tax_rule_group()
+        {
+        }
+
+    }
+}
