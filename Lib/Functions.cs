be722593dc8faf810763d003264a2d03a3044c42
Eduardo <eduardo.leon.pavon@gmail.com>
2014-09-27 19:00:23 +0200

    Issue #81 fixed. Thanks to @mvanrijnen

--- Lib/Functions.cs
+++ Lib/Functions.cs
@@ -7,13 +7,27 @@ using System.Threading.Tasks;
 
 namespace Bukimedia.PrestaSharp.Lib
 {
-    public class Functions
+    internal static class Functions
     {
-        public static string BuildLinkRewrite(string InputString)
+        internal static string BuildLinkRewrite(string InputString)
         {
             Regex regex = new Regex("[^a-zA-Z0-9-]");
             
             return regex.Replace(InputString, "-").ToLower();
+        }
+
+        internal static string ReplaceFirstOccurrence(string Source, string Find, string Replace)
+        {
+            int Place = Source.IndexOf(Find);
+            string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+            return result;
+        }
+
+        internal static string ReplaceLastOccurrence(string Source, string Find, string Replace)
+        {
+            int Place = Source.LastIndexOf(Find);
+            string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+            return result;
         }
     }
 }
