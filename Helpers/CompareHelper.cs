b04fc126c2bfcc63252cd0660dd6ac4a5d4b25ac
Eduardo <eduardo.leon.pavon@gmail.com>
2014-10-13 11:33:09 +0200

    Added helper namespace. Thanks to @dawidcieszynski
    Added CompareHelper and LanguageHellper.

---
+++ Helpers/CompareHelper.cs
@@ -0,0 +1,43 @@
+﻿using Bukimedia.PrestaSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bukimedia.PrestaSharp.Helpers
+{
+    public static class CompareHelper
+    {
+        public static IEnumerable<string> DifferentValueFields(product LeftProduct, product RightProduct)
+        {
+            var properties = new List<string>();
+            foreach (var propertyInfo in LeftProduct.GetType().GetProperties())
+            {
+                if (propertyInfo.Name.StartsWith("id") || propertyInfo.Name.StartsWith("associations"))
+                {
+                    continue;
+                }
+                var type = propertyInfo.PropertyType;
+                if (type.Name.StartsWith("Nullable"))
+                {
+                    type = type.GenericTypeArguments[0];
+                }
+                object leftNullableValue = propertyInfo.GetValue(LeftProduct);
+                object rightNullableValue = propertyInfo.GetValue(RightProduct);
+                var leftValue = leftNullableValue == null ? null : Convert.ChangeType(leftNullableValue, type);
+                var rightValue = rightNullableValue == null ? null : Convert.ChangeType(rightNullableValue, type);
+                if (type == typeof(List<Bukimedia.PrestaSharp.Entities.AuxEntities.language>))
+                {
+                    leftValue = LanguageHelper.GetString(leftValue as IEnumerable<Bukimedia.PrestaSharp.Entities.AuxEntities.language>);
+                    rightValue = LanguageHelper.GetString(rightValue as IEnumerable<Bukimedia.PrestaSharp.Entities.AuxEntities.language>);
+                }
+                if (!Equals(leftValue, rightValue))
+                {
+                    properties.Add(propertyInfo.Name);
+                }
+            }
+            return properties;
+        }
+    }
+}
---
+++ Helpers/LanguageHelper.cs
@@ -0,0 +1,17 @@
+﻿using Bukimedia.PrestaSharp.Entities.AuxEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bukimedia.PrestaSharp.Helpers
+{
+    public static class LanguageHelper
+    {
+        public static string GetString(IEnumerable<language> LanguageValues)
+        {
+            return string.Join("/", LanguageValues.Select(n => n.Value));
+        }
+    }
+}
