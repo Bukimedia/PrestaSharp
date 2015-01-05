806634f1a2a9e9e7e35ddbd58736a664c9876c4f
gryzzli <adam@grzybowice.pl>
2015-01-05 16:47:24 +0100

    Create tax.cs

---
+++ Entities/FilterEntities/tax.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities.FilterEntities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities/FilterEntities")]
+    public class tax : GenericFilterEntity
+    {
+    }
+}
