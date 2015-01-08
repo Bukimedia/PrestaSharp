ccb801cf6ccf5c2285f2565eb1526f01c07aedf6
Eduardo <eduardo.leon.pavon@gmail.com>
2015-01-08 17:23:31 +0100

    Revert "Revert "Added tax and tax_rule support. Thanks to @gryzzli""
    This reverts commit 15207e21462521a5517edf60295487c847888176.

---
+++ Entities/FilterEntities/tax_rule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace Bukimedia.PrestaSharp.Entities.FilterEntities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities/FilterEntities")]
+    public class tax_rule : GenericFilterEntity
+    {
+    }
+}
