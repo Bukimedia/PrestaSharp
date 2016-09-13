467c553020602eaf2ca5a7106994beea4860ea1b
Mark Beazley <mark@bluebanana.com>
2016-09-13 13:56:06 +0100

    Update ProductSupplierFactory to add missing Methods.

--- Factories/ProductSupplierFactory.cs
+++ Factories/ProductSupplierFactory.cs
@@ -20,12 +20,6 @@ namespace Bukimedia.PrestaSharp.Factories
             return this.Execute<Entities.product_supplier>(request);
         }
 
-        public List<long> GetIds()
-        {
-            RestRequest request = this.RequestForGet("product_suppliers", null, "prestashop");
-            return this.ExecuteForGetIds<List<long>>(request, "product_supplier");
-        }
-
         public Entities.product_supplier Add(Entities.product_supplier ProductSupplier)
         {
             long? idAux = ProductSupplier.id;
@@ -53,6 +47,48 @@ namespace Bukimedia.PrestaSharp.Factories
         public void Delete(Entities.product_supplier ProductSupplier)
         {
             this.Delete((long)ProductSupplier.id);
+        }
+
+        public List<long> GetIds()
+        {
+            RestRequest request = this.RequestForGet("product_suppliers", null, "product_suppliers");
+            return this.ExecuteForGetIds<List<long>>(request, "product_supplier");
+        }
+
+        /// <summary>
+        /// More information about filtering: http://doc.prestashop.com/display/PS14/Chapter+8+-+Advanced+Use
+        /// </summary>
+        /// <param name="Filter">Example: key:name value:Apple</param>
+        /// <param name="Sort">Field_ASC or Field_DESC. Example: name_ASC or name_DESC</param>
+        /// <param name="Limit">Example: 5 limit to 5. 9,5 Only include the first 5 elements starting from the 10th element.</param>
+        /// <returns></returns>
+        public List<Entities.product_supplier> GetByFilter(Dictionary<string, string> Filter, string Sort, string Limit)
+        {
+            RestRequest request = this.RequestForFilter("product_suppliers", "full", Filter, Sort, Limit, "product_suppliers");
+            return this.ExecuteForFilter<List<Entities.product_supplier>>(request);
+        }
+
+        /// <summary>
+        /// More information about filtering: http://doc.prestashop.com/display/PS14/Chapter+8+-+Advanced+Use
+        /// </summary>
+        /// <param name="Filter">Example: key:name value:Apple</param>
+        /// <param name="Sort">Field_ASC or Field_DESC. Example: name_ASC or name_DESC</param>
+        /// <param name="Limit">Example: 5 limit to 5. 9,5 Only include the first 5 elements starting from the 10th element.</param>
+        /// <returns></returns>
+        public List<long> GetIdsByFilter(Dictionary<string, string> Filter, string Sort, string Limit)
+        {
+            RestRequest request = this.RequestForFilter("product_suppliers", "[id]", Filter, Sort, Limit, "product_suppliers");
+            List<PrestaSharp.Entities.FilterEntities.product_supplier> aux = this.Execute<List<PrestaSharp.Entities.FilterEntities.product_supplier>>(request);
+            return (List<long>)(from t in aux select t.id).ToList<long>();
+        }
+
+        /// <summary>
+        /// Get all product suppliers.
+        /// </summary>
+        /// <returns>A list of product suppliers</returns>
+        public List<Entities.product_supplier> GetAll()
+        {
+            return this.GetByFilter(null, null, null);
         }
 
         public List<Entities.product_supplier> AddList(List<Entities.product_supplier> ProductSuppliers)
