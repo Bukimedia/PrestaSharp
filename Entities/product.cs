3d3e67f837601701acf18f9c4cceccf529cfeed7
Eduardo León Pavón <eduardo.leon@bukimedia.com>
2017-03-24 17:04:46 +0100

    Product entity updated for PrestaShop 1.7

--- Entities/product.cs
+++ Entities/product.cs
@@ -11,31 +11,23 @@ namespace Bukimedia.PrestaSharp.Entities
     [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities")]
     public class product : PrestaShopEntity
     {
-        public long? id { get; set; }
-        public long? id_supplier { get; set; }
+        public long? id { get; set; } 
         public long? id_manufacturer { get; set; }
+        public long? id_supplier { get; set; }
         public long? id_category_default { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int? New { get; set; }
+        public long? cache_default_attribute { get; set; }
         public long? id_default_image { get; set; }
         public long? id_default_combination { get; set; }
         public long? id_tax_rules_group { get; set; }
+        public long? position_in_category { get; set; }
+        public string manufacturer_name { get; }
+        public long quantity { get; }
+        public string type { get; set; }
         public long id_shop_default { get; set; }
-        /// <summary>
-        /// It´s a logical bool.
-        /// </summary>
-        public int on_sale { get; set; }
-        /// <summary>
-        /// It´s a logical bool.
-        /// </summary>
-        public int online_only { get; set; }
-        public string ean13 { get; set; }
-        public string upc { get; set; }
-        public decimal ecotax { get; set; }
-        public long minimal_quantity { get; set; }
-        public decimal price { get; set; }
-        public decimal wholesale_price { get; set; }
-        public string unity { get; set; }
-        public decimal unit_price_ratio { get; set; }
-        public decimal additional_shipping_cost { get; set; }
         public string reference { get; set; }
         public string supplier_reference { get; set; }
         public string location { get; set; }
@@ -44,9 +36,40 @@ namespace Bukimedia.PrestaSharp.Entities
         public decimal depth { get; set; }
         public decimal weight { get; set; }
         public int? quantity_discount { get; set; }
+        public string ean13 { get; set; }
+        public string upc { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int cache_is_pack { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int cache_has_attachments { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int is_virtual { get; set; }
+
+        public long state { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int on_sale { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int online_only { get; set; }        
+        public decimal ecotax { get; set; }
+        public long minimal_quantity { get; set; }
+        public decimal price { get; set; }
+        public decimal wholesale_price { get; set; }
+        public string unity { get; set; }
+        public decimal unit_price_ratio { get; set; }
+        public decimal additional_shipping_cost { get; set; }   
         public sbyte customizable { get; set; }
-        public sbyte uploadable_files { get; set; }
         public sbyte text_fields { get; set; }
+        public sbyte uploadable_files { get; set; }        
         /// <summary>
         /// It´s a logical bool.
         /// </summary>
@@ -62,6 +85,10 @@ namespace Bukimedia.PrestaSharp.Entities
         /// It´s string because you can receive a string with no DateTime .Net format.
         /// </summary>
         public string available_date { get; set; }
+        /// <summary>
+        /// It´s a logical bool.
+        /// </summary>
+        public int show_condition { get; set; }
         public string condition { get; set; }
         /// <summary>
         /// It´s a logical bool.
@@ -75,16 +102,7 @@ namespace Bukimedia.PrestaSharp.Entities
         /// <summary>
         /// It´s a logical bool.
         /// </summary>
-        public int cache_is_pack { get; set; }
-        /// <summary>
-        /// It´s a logical bool.
-        /// </summary>
-        public int cache_has_attachments { get; set; }
-        /// <summary>
-        /// It´s a logical bool.
-        /// </summary>
-        public int is_virtual { get; set; }
-        public long? cache_default_attribute { get; set; }
+        public int advanced_stock_management { get; set; }
         /// <summary>
         /// It´s a logical DateTime. Format YYYY-MM-DD HH:MM:SS.
         /// </summary>
@@ -93,10 +111,7 @@ namespace Bukimedia.PrestaSharp.Entities
         /// It´s a logical DateTime. Format YYYY-MM-DD HH:MM:SS.
         /// </summary>
         public string date_upd { get; set; }
-        /// <summary>
-        /// It´s a logical bool.
-        /// </summary>
-        public int advanced_stock_management { get; set; }
+        public long pack_stock_type { get; set; }
         public List<Entities.AuxEntities.language> meta_description { get; set; }
         public List<Entities.AuxEntities.language> meta_keywords { get; set; }
         public List<Entities.AuxEntities.language> meta_title { get; set; }
