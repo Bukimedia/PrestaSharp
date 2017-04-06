86e0d846eca6e30d8fc68d9219648de149beff68
Juan Ignacio García Pascual <juan.ignacio.garcia@bukimedia.com>
2017-04-06 10:25:51 +0200

    Introduction of GenericFactory. Refactorization of ProductFactory to inherit from GenericFactory.

---
+++ Entities/PrestaShopFactoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities")]
+    public interface IPrestaShopFactoryEntity
+    {
+        long? id { get; set; }
+    }
+}
