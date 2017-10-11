720077fa2df9cd258da983b7d30204e476030a49
Eduardo León Pavón <eduardo.leon@bukimedia.com>
2017-10-11 14:53:26 +0200

    Corrected typo in ProductFeatureFactory.cs

--- Factories/ProductFeatureFactory.cs
+++ Factories/ProductFeatureFactory.cs
@@ -11,8 +11,8 @@ namespace Bukimedia.PrestaSharp.Factories
 {
     public class ProductFeatureFactory : GenericFactory<product_feature>
     {
-        protected override string singularEntityName { get { return "product_feture"; } }
-        protected override string pluralEntityName { get { return "product_fetures"; } }
+        protected override string singularEntityName { get { return "product_feature"; } }
+        protected override string pluralEntityName { get { return "product_features"; } }
 
         public ProductFeatureFactory(string BaseUrl, string Account, string SecretKey)
             : base(BaseUrl, Account, SecretKey)
