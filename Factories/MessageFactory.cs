47cefb3ad832983b8227e418ade67b51ecc97fcc
sanctusmob <xarilaos.spam@gmail.com>
2018-11-20 09:28:33 +0200

    Order Message Support

---
+++ Factories/MessageFactory.cs
@@ -0,0 +1,22 @@
+﻿using Bukimedia.PrestaSharp.Entities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Factories
+{
+    public class MessageFactory : GenericFactory<message>
+    {
+        protected override string singularEntityName { get { return "message"; } }
+        protected override string pluralEntityName { get { return "messages"; } }
+
+        public MessageFactory(string BaseUrl, string Account, string SecretKey)
+            : base(BaseUrl, Account, SecretKey)
+        {
+        }
+    }
+}
