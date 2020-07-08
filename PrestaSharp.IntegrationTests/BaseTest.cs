3d10870cca073230277489772994b4e694ba1411
Olivier ROMAND <oromand@instore-solution.fr>
2020-07-08 08:53:34 +0200

    Changed configuration

--- PrestaSharp.IntegrationTests/BaseTest.cs
+++ PrestaSharp.IntegrationTests/BaseTest.cs
@@ -2,7 +2,7 @@ namespace PrestaSharp.IntegrationTests
 {
     public class BaseTest
     {
-        protected const string TestUrl = "http://localhost:8080/api";
-        protected const string TestApiKey = "F9HRC3MTYYSLC98C57HCHRHAHPRFINMB";
+        protected const string TestUrl = "http://prestashop.local/api";
+        protected const string TestApiKey = "GLPLF5YF3S3UW9X7U4EWCP5ABXQUJ6PW";
     }
 }
