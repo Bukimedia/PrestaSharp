22a4ded055ec644e0246966d1c1ea64ad5f2c27e
Jean-Pierre Sneyers <Jean-Pierre.Sneyers@snel.be>
2020-08-19 14:05:29 +0200

    Add customized factory

---
+++ PrestaSharp/Entities/customized_data_image.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities")]
+    public class customized_data_image : PrestaShopEntity
+    {
+        public long? id { get; set; }
+        public long id_customization_field { get; set; }
+        public string value { get; set; }
+
+        public customized_data_image()
+        {
+
+        }
+    }
+}
