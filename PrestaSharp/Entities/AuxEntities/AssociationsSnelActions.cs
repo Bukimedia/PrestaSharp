2af1d4748303eb0d7511a9ab0feb31fc39b1537c
SnelGrafics <45786248+SnelGrafics@users.noreply.github.com>
2021-03-26 11:05:27 +0100

    Add files via upload

---
+++ PrestaSharp/Entities/AuxEntities/AssociationsSnelActions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities.AuxEntities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities/AuxEntities")]
+    public class AssociationsSnelAction : PrestaShopEntity
+    {
+        public List<snelRunDetail> snelRunDetails { get; set; }
+
+        public AssociationsSnelAction()
+        {
+            this.snelRunDetails = new List<snelRunDetail>();
+        }
+    }
+}
---
+++ PrestaSharp/Entities/AuxEntities/snelRunDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Entities.AuxEntities
+{
+    [XmlType(Namespace = "Bukimedia/PrestaSharp/Entities/AuxEntities")]
+    public class snelRunDetail : GenericAssociation
+    {
+        //public snelRunDetail();
+    }
+}
