30a4789c7ed27090392e5bb6292d045b55a4d10d
The-mob Sanctus <xarilaos.im@gmail.com>
2024-02-16 12:45:29 +0200

    Add Image to Suppliers

---
+++ PrestaSharp/Factories/BoxnowEntryFactory.cs
@@ -0,0 +1,22 @@
+﻿using Bukimedia.PrestaSharp.Entities;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bukimedia.PrestaSharp.Factories
+{
+    public class BoxnowEntryFactory : GenericFactory<boxnow_entry>
+    {
+        protected override string singularEntityName { get { return "boxnow_entry"; } }
+        protected override string pluralEntityName { get { return "boxnow_entries"; } }
+
+        public BoxnowEntryFactory(string BaseUrl, string Account, string SecretKey)
+            : base(BaseUrl, Account, SecretKey)
+        {
+        }    
+    }
+}
